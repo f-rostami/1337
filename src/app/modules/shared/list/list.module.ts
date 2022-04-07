4b61519f5c8847c8417575462cc16b80e0c47059
farbod rostami <rostami.farbod@outlook.com>
2022-04-07 12:38:32 +0430

    create primitive structure and developed a list module structure with the ability of reusability and a list's UI. Using the app component for playground

---
+++ src/app/modules/shared/list/list.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ListComponent } from './list.component';
+import { HeaderModule } from './components/header/header.module';
+import { ContentModule } from './components/content/content.module';
+
+
+
+@NgModule({
+  declarations: [
+    ListComponent
+  ],
+  imports: [
+    CommonModule,
+    HeaderModule,
+    ContentModule
+  ],
+  exports: [
+    ListComponent
+  ]
+})
+export class ListModule { }
